d126ee2d2d0900056ecbfe778ff6d10beeec1099
Annie Li <liannie6241@gmail.com>
2022-06-28 15:32:03 -0500

    ejkgeiwgkfew

--- MyChoice/ViewController.swift
+++ MyChoice/ViewController.swift
@@ -9,14 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func button(_ sender: Any) {
-        label.text = "yes you are so smart"
-    }
+    
+    
+    
 }
 
 
