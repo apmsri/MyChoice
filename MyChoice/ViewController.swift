d3654e3707f261cf20848bc3a4c27b4d7b3ed425
Aahana Mallela <aahanasree@gmail.com>
2022-06-29 10:29:32 -0500

    few

--- MyChoice/ViewController.swift
+++ MyChoice/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+    @IBAction func articleOneButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://voiceoffrisco.com/land-of-the-free-just-got-a-little-less-free-the-overturn-of-roe-v-wade/")! as URL, options: [:], completionHandler: nil)
+    }
     
+    @IBAction func articleTwoButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://news.trust.org/item/20220627135508-zoim3")! as URL, options: [:], completionHandler: nil)
+    }
     
-    
+    @IBAction func articleThreeButton(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://news.trust.org/item/20220624180120-flqzx")! as URL, options: [:], completionHandler: nil)
+    }
 }
 
 
