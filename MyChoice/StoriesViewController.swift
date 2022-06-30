f9bdf0418fce838141f991436229a4660909093f
EllePace <elleleepace@gmail.com>
2022-06-30 16:37:41 -0500

    m

---
+++ MyChoice/StoriesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StoriesViewController.swift
+//  MyChoice
+//
+//  Created by Scholar on 6/30/22.
+//
+
+import UIKit
+
+class StoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
